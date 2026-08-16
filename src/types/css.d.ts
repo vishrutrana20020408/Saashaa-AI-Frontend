59dc7b89dd96987ca18694b3b8b4b29715044384
Vishrut Rana <99553192+vishrutrana20020408@users.noreply.github.com>
2026-08-16 21:12:06 +0530

    Add type declarations for CSS and Sass file imports in TypeScript

---
+++ src/types/css.d.ts
@@ -0,0 +1,7 @@
+// Type declarations for importing CSS/Sass files in TypeScript
+declare module '*.css';
+declare module '*.module.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.module.scss';
+declare module '*.module.sass';
